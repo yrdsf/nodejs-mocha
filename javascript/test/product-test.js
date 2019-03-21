983a8d1416da0b222336c57fb4685901b3c1461a
Yanire Romero <yanire.romero@gmail.com>
2019-03-21 16:48:27 -0500

    chai test

--- javascript/test/product-test.js
+++ javascript/test/product-test.js
@@ -1,12 +1,28 @@
-var expect  = require('chai').expect;
-var request = require('request');
+import chai from "chai";
+import http from "chai-http";
 
-it('Get Product status', function(done) {
-    request.get('http://localhost:3000')
-     .on('response', function(response) {
-        console.log(response);
-        //console.log(response._events.response);
-        expect(response.statusCode).to.equal(200);
-        done();
+import index from "../src/app";
+
+const expect = chai.expect;
+const app = index.listen();
+
+chai.use(http);
+
+describe("Unit testing AQT", () => {
+  after(() => app.close);
+
+  describe("GET /", () => {
+    it("return a list of products", async () => {
+      const res = await chai.request(app).get("/");
+
+      expect(res).to.have.status(200);
     });
+  });
+
+  describe("404 error", () => {
+    it("return category by incorrect iso", async () => {
+      const res = await chai.request(app).get("/NOT_FOUND/404");
+      expect(res).to.have.status(404);
+    });
+  });
 });
