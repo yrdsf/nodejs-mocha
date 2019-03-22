36a61f0d400de258e425f663501000afb13f82b0
Yanire Romero <yanire.romero@gmail.com>
2019-03-22 08:38:27 -0500

    coverage net

---
+++ net/net-nunit/net-nunit.Tests/ProductTest.cs
@@ -0,0 +1,24 @@
+﻿using netnunit.Controllers;
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace netnunit.Tests
+{
+    [TestFixture()]
+    public class ProductTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+            var controller = new ProductController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            var response = controller.Get();
+
+            Assert.AreEqual("Hello", response[0]);
+        }
+    }
+}
